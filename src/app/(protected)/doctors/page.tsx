2c6b48fb39222cd6800f57e1c0573503867e525b
Jorge Wendell Correia <jorge.wendell@adelbr.tech>
2025-05-30 10:28:00 -0300

    feat: add doctor creation form

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,8 +1,6 @@
-import { PlusIcon } from "lucide-react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
 import {
   PageActions,
   PageContainer,
@@ -13,6 +11,8 @@ import {
   PageTitle,
 } from "@/components/ui/page-container";
 import { auth } from "@/lib/auth";
+
+import AddDoctorButton from "./components/add-doctor-button";
 
 const DoctorsPage = async () => {
   const session = await auth.api.getSession({
@@ -34,10 +34,7 @@ const DoctorsPage = async () => {
           </PageDescriptions>
         </PageHeaderContent>
         <PageActions>
-          <Button>
-            <PlusIcon className="h-4 w-4" />
-            Novo medico
-          </Button>
+          <AddDoctorButton />
         </PageActions>
       </PageHeader>
       <PageContent>
